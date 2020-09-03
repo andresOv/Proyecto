42c1a6cacd5a42fd6395523145fbb8b5ae83d5ef
andresOv <50975345+andresOv@users.noreply.github.com>
2020-09-02 23:37:25 -0600

    prueba3

--- FacturaDigital/src/facturadigital/FacturaDigital.java
+++ FacturaDigital/src/facturadigital/FacturaDigital.java
@@ -15,7 +15,7 @@ public class FacturaDigital {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      System. out. println("hasta nunca mundo");
+      System. out. println("andres");
     }
     
 }
